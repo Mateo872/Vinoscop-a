62a91665b11f72b2aff2c9214b3b658049e3e1b5
Mateo Bellini <bellinimateo872@gmail.com>
2023-04-26 09:42:34 -0300

    Animación en el header hecha

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 const iconHamburger = document.querySelector(".bi-list"),
   navContainer = document.querySelector(".nav_container"),
   iconClose = document.querySelector(".bi-x"),
+  nav = document.querySelector("nav"),
+  link_header = document.querySelectorAll(".link_header"),
   ul = document.querySelector("ul");
 
 navContainer.addEventListener("click", (e) => {
@@ -20,3 +22,19 @@ function close() {
   navContainer.style.visibility = "hidden";
   ul.classList.remove("menu_visible");
 }
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 75) {
+    nav.style.backgroundColor = "#ff8500";
+    ul.style.backgroundColor = "#fff";
+    ul.style.color = "#000";
+    iconClose.style.color = "#000";
+    link_header.forEach((link) => (link.style.color = "#000"));
+  } else {
+    nav.style.backgroundColor = "transparent";
+    ul.style.backgroundColor = "#ff8500";
+    ul.style.color = "#fff";
+    iconClose.style.color = "#fff";
+    link_header.forEach((link) => (link.style.color = "#fff"));
+  }
+});
